a9b2b6b68c42afad5c25f4f1def17ba3592610c3
Nurzhan <nur42097@gmail.com>
2020-03-31 11:54:14 +0600

    Money_Keeper

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+var start = document.getElementById('start'),
+    budjet_value= document.querySelector(".budjet-value"),
+    daybudget_value = document.querySelector(".daybudget-value"),
+    level_value= document.querySelector(".level-value"),
+    expenses_value= document.querySelector(".expenses-value"),
+    optionalexpenses_value= document.querySelector(".optionalexpenses-value"),
+    income_value= document.querySelector(".income-value"),
+    monthsavings_value= document.querySelector(".monthsavings-value"),
+    yearsavings_value= document.querySelector(".yearsavings-value"),
+    expenses_item = document.querySelectorAll('.expenses-item'),
+    button = document.getElementsByTagName('button'),
+    optionalexpenses_item = document.querySelectorAll('.optionalexpenses-item'),
+    choose_income = document.querySelector('.choose-income'),
+    checksavings = document.querySelector('.checksavings'),
+    choose_sum = document.querySelector('.choose-sum'),
+    choose_percent = document.querySelector('.choose-percent'),
+    year_value = document.querySelector('.year-value'),
+    month_value = document.querySelector('.month-value'),
+    day_value = document.querySelector('.day-value');
+
+
+var expenses_item_btn = button[0],
+    optionalexpenses_btn = button[1],
+    count_budget_btn = button[2];
+
+
+
+
+
+
+
+
+
+
+console.log(expenses_item_btn);
